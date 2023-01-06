8bf425651cafeec986afa88ca5ae3876298080c0
Levi Lessa <levijr.santos@gmail.com>
2023-01-06 18:49:07 -0300

    adding google fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+import { useFonts } from 'expo-font'
+import { Lora_500Medium } from '@expo-google-fonts/lora'
+import { Montserrat_400Regular } from '@expo-google-fonts/montserrat'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Lora_500Medium, Montserrat_400Regular })
+
+  if (!fontsLoaded) return <View />
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
